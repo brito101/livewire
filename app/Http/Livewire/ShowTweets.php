c110b7f79b59bccfea92987785bd620ea89a30af
brito101 <rodrigobrito101@hotmail.com>
2022-06-20 11:42:17 -0300

    ADD model LIKE

--- app/Http/Livewire/ShowTweets.php
+++ app/Http/Livewire/ShowTweets.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use App\Models\Tweet;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -17,16 +18,16 @@ class ShowTweets extends Component
 
     public function render()
     {
-        $tweets = Tweet::with('user')->paginate(2);
+        $tweets = Tweet::with('user')->latest()->paginate(10);
         return view('livewire.show-tweets', compact('tweets'));
     }
 
     public function create()
     {
         $this->validate();
-        Tweet::create([
-            'content' => $this->content,
-            'user_id' => 1
+
+        Auth::user()->tweets()->create([
+            'content' => $this->content
         ]);
 
         $this->content = "";
--- resources/views/livewire/show-tweets.blade.php
+++ resources/views/livewire/show-tweets.blade.php
@@ -12,6 +12,11 @@
     <hr />
     @foreach ($tweets as $tweet)
         <p>{{ $tweet->user->name }} - {{ $tweet->content }}</p>
+        @if ($tweet->likes->count())
+            <a href="">Descurtir</a>
+        @else
+            <a href="">Curtir</a>
+        @endif
     @endforeach
 
     <hr />

2a4c7253ee238a1564293f6595b1d900b824fb5e
brito101 <rodrigobrito101@hotmail.com>
2022-06-20 11:07:30 -0300

    Component setting

---
+++ app/Http/Livewire/ShowTweets.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Tweet;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class ShowTweets extends Component
+{
+    use WithPagination;
+
+    public $content;
+    protected $rules = [
+        'content' => 'required|min:3|max:255'
+    ];
+
+    public function render()
+    {
+        $tweets = Tweet::with('user')->paginate(2);
+        return view('livewire.show-tweets', compact('tweets'));
+    }
+
+    public function create()
+    {
+        $this->validate();
+        Tweet::create([
+            'content' => $this->content,
+            'user_id' => 1
+        ]);
+
+        $this->content = "";
+    }
+}
---
+++ resources/views/livewire/show-tweets.blade.php
@@ -0,0 +1,21 @@
+<div>
+    Teste
+    <p>{{ $content }}</p>
+    <form method="post" wire:submit.prevent="create">
+        <input type="text" name="content" id="content" wire:model="content">
+        @error('content')
+            {{ $message }}
+        @enderror
+        <button type="submit">Criar Tweet</button>
+    </form>
+
+    <hr />
+    @foreach ($tweets as $tweet)
+        <p>{{ $tweet->user->name }} - {{ $tweet->content }}</p>
+    @endforeach
+
+    <hr />
+    <div>
+        {{ $tweets->links() }}
+    </div>
+</div>
